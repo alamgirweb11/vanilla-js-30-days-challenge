1e4f0efda930c8148bc571a763b2f31a5dfc96af
alamgirweb11 <alamgirweb11@gmail.com>
2022-11-22 23:44:38 +0600

    video play and pause method added

---
+++ 11-custom-video-player/script.js
@@ -0,0 +1,26 @@
+// get all elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+// functions
+function togglePlay(){
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton(){
+       const icon = this.paused ? '►' : '❚ ❚';
+       toggle.textContent = icon;
+}
+
+// event listener
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
