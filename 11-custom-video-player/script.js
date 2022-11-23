a5a9d3b9e3a54b8f3ea09ecad981528ad940b7a8
alamgirweb11 <alamgirweb11@gmail.com>
2022-11-23 23:57:14 +0600

    video duration progress drag and move

--- 11-custom-video-player/script.js
+++ 11-custom-video-player/script.js
@@ -30,6 +30,12 @@ function handleRangeUpdate(){
     video[this.name] = this.value;
 }
 
+// move video duration progress
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 // event listener
 // pause and play event
 video.addEventListener('click', togglePlay);
@@ -41,3 +47,10 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+// video duration drag and move 
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
