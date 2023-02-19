cf38336d5819720fa8de6fc980ce4872a9bb7537
alamgirweb11 <alamgirweb11@gmail.com>
2023-02-19 23:39:02 +0600

    add paintToCanvas method

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -11,8 +11,20 @@ function getVideo() {
         video.play();
      })
      .catch(err => {
-         console.log(err);
+         console.log(`error: ${err}`);
      })
+}
+
+function paintToCanvas() {
+      const width = video.videoWidth;
+      const height = video.videoHeight;
+
+      canvas.width = width;
+      canvas.height = height;
+
+    return  setInterval(() => {
+          ctx.drawImage(video, 0, 0, width, height);
+      }, 16);
 }
 
 getVideo();
