f20c27a9e28b75b7769ba8e5807cafa5cf0d7939
alamgirweb11 <alamgirweb11@gmail.com>
2023-02-20 23:39:05 +0600

    take photo from camera

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -27,4 +27,20 @@ function paintToCanvas() {
       }, 16);
 }
 
+function takePhoto() {
+      snap.currentTime = 0;
+      snap.play();
+
+      // take the data out of the canvas
+      const data = canvas.toDataURL('image/jpeg');
+      const link = document.createElement('a');
+      link.href = data;
+      link.setAttribute('download', 'camera_pic');
+      link.innerHTML = `<img src="${data}" alt="camera pic" />`
+      link.textContent = 'Download Image';
+      strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
