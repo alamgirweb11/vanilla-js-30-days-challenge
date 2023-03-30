8917c573718f258584a2f770dc6c1428133c7d57
alamgirweb11 <alamgirweb11@gmail.com>
2023-03-30 23:54:36 +0600

    another new function added

--- 29-countdown-timer/script.js
+++ 29-countdown-timer/script.js
@@ -1,5 +1,19 @@
 function timer(seconds){
   const now = Date.now();
   const then = now + seconds * 1000;
-  console.log({now, then});
+  
+  countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      // check if we should stop it
+      if(secondsLeft <= 0) {
+         clearInterval(countdown);
+         return;
+      }
+      // display it
+      displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    console.log(seconds);
 }
